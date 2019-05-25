8fcf2a97c5f3b6ffd683a6061f8691b566ed9efc
Ritwick Dey (from Travis CI) <ritwickdey@outlook.com>
2019-05-25 15:21:33 +0000

    Deploy ritwickdey97/greedy-game-assignment to github.com/ritwickdey97/greedy-game-assignment.git:gh-pages

---
+++ precache-manifest.e801794e0abb3a79fad6b3a243af397d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e03fbcb80fdf024c4e57bfa31c9aa2",
+    "url": "/greedy-game-assignment/index.html"
+  },
+  {
+    "revision": "362ce7998c1ceef798a3",
+    "url": "/greedy-game-assignment/static/css/2.ef05a01a.chunk.css"
+  },
+  {
+    "revision": "ac2e1dd295235b10d230",
+    "url": "/greedy-game-assignment/static/css/main.3193cc24.chunk.css"
+  },
+  {
+    "revision": "362ce7998c1ceef798a3",
+    "url": "/greedy-game-assignment/static/js/2.61bc4f90.chunk.js"
+  },
+  {
+    "revision": "ac2e1dd295235b10d230",
+    "url": "/greedy-game-assignment/static/js/main.b7d54f62.chunk.js"
+  },
+  {
+    "revision": "e2d0d36a306e57e7080d",
+    "url": "/greedy-game-assignment/static/js/runtime~main.ef411df4.js"
+  }
+]);
